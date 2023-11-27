62aa7e04bf734fb85aa8dfaed62f509e9d3d5f7c
raghu-nandan10 <trafalgardwaterlaw393@gmail.com>
2023-11-27 15:21:42 +0530

    wip

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import axios from "axios";
+import { io } from "socket.io-client";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [socket, setSocket] = useState(
+    io("http://localhost:4000", {
+      withCredentials: true,
+    })
+  );
+  useEffect(() => {
+    const fetchCookie = async () => {
+      try {
+        const res = await axios.get("http://localhost:4000/get-cookie", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        });
+        console.log(res.data);
+        socket.emit("send:cookie", {});
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCookie();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
